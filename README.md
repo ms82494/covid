9d2e72d483d4bf9853d94f0556a0f29b811276ad
ms82494 <6218876+ms82494@users.noreply.github.com>
2020-03-09 16:59:02 -0700

    reran with 3/9/20 data

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This repo contains [COVID-19]('.COVID-19/'), which is a repo with global case
+This repo contains [COVID-19](https://github.com/CSSEGISandData/COVID-19/tree/4a0db2ab780e3e83ff607a0b78810fdcc425f0b6), which is a repo with global case
 data of infections with severe acute respiratory syndrome coronavirus 2
 (SARS-CoV-2), which was initially named COVID-19. This repository is maintained
 by the Center for Systems Science and Engineering at Johns Hopkins University
@@ -9,7 +9,7 @@ the source. No warranties, of course.
 
 The only problem I had with the data as presented on the COVID-19 website
 created by JHU CSSE is that it doesn't make it easy to get a glimpse at the
-spread of the disease through time _in a particular country/region_.
+spread of the disease through time *and* in a particular country/region.
 
 To facilitate this, I created the Python script in the folder [unpivot](./unpivot).
 I wanted to have the data in a single csv or Excel file that would include a
--- unpivot/covid.py
+++ unpivot/covid.py
@@ -3,7 +3,7 @@
 import pandas as pd
 from pathlib import Path
 
-datadirectory = Path().cwd().parent / 'csse_covid_19_data' / 'csse_covid_19_time_series'
+datadirectory = Path().cwd().parent / 'COVID-19' / 'csse_covid_19_data' / 'csse_covid_19_time_series'
 
 CONFNM = 'time_series_19-covid-Confirmed.csv'
 DEATHSNM = 'time_series_19-covid-Deaths.csv'
@@ -16,6 +16,7 @@ RECOFILE = datadirectory / RECONM
 conf = pd.read_csv(CONFFILE)
 deaths = pd.read_csv(DEATHSFILE)
 reco = pd.read_csv(RECOFILE)
+
 
 def make_long(df, varname):
     """
@@ -33,23 +34,25 @@ def make_long(df, varname):
     df_long : the unpivoted dataframe
 
     """
-    
+
     # create list of dates to turn into value variables
     dates = list(df.columns[4:])
-    
+
     df.rename(columns={'Province/State': 'region',
                        'Country/Region': 'country'}, inplace=True)
-    
+
     # create list of id variables for the unpivot
     ids = list(df.columns[0:4])
-    
-    df_long = pd.melt(df, id_vars=ids, value_vars=dates,
-                   var_name='date', value_name=varname)
-    
+
+    df_long = pd.melt(df, id_vars=ids,
+                      value_vars=dates,
+                      var_name='date',
+                      value_name=varname)
+
     # in order to merge confirmed, recovered, deaths files later on,
     # missing region values are converted to empty strings.
     df_long['region'] = df_long['region'].fillna('')
-    
+
     # I don't need latitude or longitude, and these cols would just get
     # duplicated in the merge, below.
     df_long.drop(['Lat', 'Long'], axis=1, inplace=True)
@@ -63,9 +66,9 @@ reco_long = make_long(reco, 'recovered')
 # combine the 3 unpivoted dataframes into one
 combo = conf_long.merge(deaths_long,
                         how='left',
-                        on=['region','country','date']).merge(
+                        on=['region', 'country', 'date']).merge(
                             reco_long,
                             how='left',
-                            on=['region','country','date'])
+                            on=['region', 'country', 'date'])
 
 combo.to_excel('unpivoted.xlsx', na_rep='N/A', index=False)
